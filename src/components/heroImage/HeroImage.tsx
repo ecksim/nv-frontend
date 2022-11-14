6916745e4dca539c325d3e269c0a1ca802f34a1c
ecksim <simonecker@web.de>
2022-11-14 22:34:00 +0100

    initial commit

---
+++ src/components/heroImage/HeroImage.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import styled from "styled-components";
+
+// components
+import Image from "next/image";
+
+// styles
+const Wrapper = styled.section`
+  position: relative;
+  width: 100%;
+  height: 250px;
+`;
+
+type Props = {};
+
+type State = {};
+
+export default class heroImage extends React.Component<Props, State> {
+  state = {};
+
+  render() {
+    return (
+      <Wrapper>
+        <Image
+          alt="Die Bodemännle auf einem Umzug"
+          src={"/headerQuadratic.jpg"}
+          fill={true}
+          style={{ objectFit: "cover", objectPosition: "center 80%" }}
+        />
+      </Wrapper>
+    );
+  }
+}
