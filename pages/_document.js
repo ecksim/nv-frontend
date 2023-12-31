f1f9b0fdc3339e335fc4cb4f4a26bcb64f990099
ecksim <simonecker@web.de>
2023-12-31 18:19:57 +0100

    page title + meta description

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="de">
-            <Head><link rel="shortcut icon" href="/favicon.png" /></Head>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.png" />
+                <title>Bodemännle Hattenweiler</title>
+                <meta property="description" content="Die Webseite des Narrenverein Bodemännle Hattenweiler. Aktuelle Neuigkeiten, Termine und Kontaktinformationen" />
+            </Head>
             <body>
                 <section className='mainWrapper'>
                     <Main />
